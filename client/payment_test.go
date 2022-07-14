e2da3318ee8fe1869519581291a758603ee56b26
Matthias Geihs <matthias@perun.network>
2022-07-14 14:57:05 +0200

    Merge payment dispute test
    Signed-off-by: Matthias Geihs <matthias@perun.network>

--- client/payment_test.go
+++ client/payment_test.go
@@ -19,6 +19,7 @@ import (
 	"math/big"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/assert"
@@ -29,6 +30,7 @@ import (
 	"github.com/perun-network/perun-eth-backend/wallet"
 
 	chtest "perun.network/go-perun/channel/test"
+	"perun.network/go-perun/client"
 	perunclient "perun.network/go-perun/client"
 	clienttest "perun.network/go-perun/client/test"
 	"perun.network/go-perun/log"
@@ -36,7 +38,12 @@ import (
 	pkgtest "polycry.pt/poly-go/test"
 )
 
-func TestHappyAliceBob(t *testing.T) {
+const (
+	twoPartyTestTimeout = 10 * time.Second
+	TxFinalityDepth     = 3
+)
+
+func TestPaymentHappy(t *testing.T) {
 	log.Info("Starting happy test")
 	rng := pkgtest.Prng(t)
 
@@ -97,4 +104,55 @@ func TestHappyAliceBob(t *testing.T) {
 	assertBal(s.Recvs[B], finalBalBob)
 
 	log.Info("Happy test done")
+}
+
+func TestPaymentDispute(t *testing.T) {
+	log.Info("Starting dispute test")
+	rng := pkgtest.Prng(t)
+
+	const A, B = 0, 1 // Indices of Mallory and Carol
+	var (
+		name  = [2]string{"Mallory", "Carol"}
+		setup [2]clienttest.RoleSetup
+		role  [2]clienttest.Executer
+	)
+
+	s := test.NewSetup(t, rng, 2, ctest.BlockInterval, TxFinalityDepth)
+	setup = ctest.MakeRoleSetups(rng, s, name)
+
+	role[A] = clienttest.NewMallory(t, setup[A])
+	role[B] = clienttest.NewCarol(t, setup[B])
+
+	execConfig := &clienttest.MalloryCarolExecConfig{
+		BaseExecConfig: clienttest.MakeBaseExecConfig(
+			[2]wire.Address{setup[A].Identity.Address(), setup[B].Identity.Address()},
+			s.Asset,
+			[2]*big.Int{big.NewInt(100), big.NewInt(1)},
+			client.WithoutApp(),
+		),
+		NumPayments: [2]int{5, 0},
+		TxAmounts:   [2]*big.Int{big.NewInt(20), big.NewInt(0)},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), twoPartyTestTimeout)
+	defer cancel()
+	clienttest.ExecuteTwoPartyTest(ctx, t, role, execConfig)
+
+	// Assert correct final balances
+	netTransfer := big.NewInt(int64(execConfig.NumPayments[A])*execConfig.TxAmounts[A].Int64() -
+		int64(execConfig.NumPayments[B])*execConfig.TxAmounts[B].Int64())
+	finalBal := [2]*big.Int{
+		new(big.Int).Sub(execConfig.InitBals()[A], netTransfer),
+		new(big.Int).Add(execConfig.InitBals()[B], netTransfer),
+	}
+	// reset context timeout
+	ctx, cancel = context.WithTimeout(context.Background(), ctest.DefaultTimeout)
+	defer cancel()
+	for i, bal := range finalBal {
+		b, err := s.SimBackend.BalanceAt(ctx, common.Address(*s.Recvs[i]), nil)
+		require.NoError(t, err)
+		assert.Zero(t, b.Cmp(bal), "ETH balance mismatch")
+	}
+
+	log.Info("Dispute test done")
 }
