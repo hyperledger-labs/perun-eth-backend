2b52c7388349ed15f7579e5d72e949c4f9800e91
Matthias Geihs <matthias@perun.network>
2022-08-09 09:51:31 +0200

    Use balances denoted in ETH instead of WEI
    Signed-off-by: Matthias Geihs <matthias@perun.network>

--- client/fund_test.go
+++ client/fund_test.go
@@ -9,14 +9,15 @@ import (
 
 	channeltest "github.com/perun-network/perun-eth-backend/channel/test"
 	ethclienttest "github.com/perun-network/perun-eth-backend/client/test"
+	"perun.network/go-perun/channel"
 	ctest "perun.network/go-perun/client/test"
 	"polycry.pt/poly-go/test"
 )
 
-func TestFund(t *testing.T) {
+func TestFundRecovery(t *testing.T) {
 	rng := test.Prng(t)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 1000*time.Second)
 	defer cancel()
 
 	ctest.TestFundRecovery(
@@ -24,13 +25,22 @@ func TestFund(t *testing.T) {
 		t,
 		ctest.FundSetup{
 			ChallengeDuration: 1,
-			FridaInitBal:      big.NewInt(100),
-			FredInitBal:       big.NewInt(50),
+			FridaInitBal:      ethToWei(big.NewFloat(100)),
+			FredInitBal:       ethToWei(big.NewFloat(50)),
+			BalanceDelta:      ethToWei(big.NewFloat(0.001)),
 		},
-		func(r *rand.Rand) [2]ctest.RoleSetup {
+		func(r *rand.Rand) ([2]ctest.RoleSetup, channel.Asset) {
 			setup := channeltest.NewSetup(t, rng, 2, ethclienttest.BlockInterval, 1)
+			for i, adj := range setup.Adjs {
+				adj.Receiver = setup.Accs[i].Account.Address
+			}
 			roles := ethclienttest.MakeRoleSetups(rng, setup, [2]string{"Frida", "Fred"})
-			return roles
+			return roles, setup.Asset
 		},
 	)
+}
+
+func ethToWei(eth *big.Float) *big.Int {
+	wei, _ := new(big.Float).Mul(eth, big.NewFloat(1e18)).Int(nil)
+	return wei
 }
