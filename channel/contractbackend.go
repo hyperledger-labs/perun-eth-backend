0e6fa0f63c5ef52d79123b6c331447ec9e540e32
NhoxxKienn <huy.tranminh2804@gmail.com>
2023-07-11 14:39:05 +0200

    add nonce-log for transactor
    Signed-off-by: NhoxxKienn <huy.tranminh2804@gmail.com>

--- channel/contractbackend.go
+++ channel/contractbackend.go
@@ -152,6 +152,7 @@ func (c *ContractBackend) NewTransactor(ctx context.Context, gasLimit uint64, ac
 	if err != nil {
 		return nil, err
 	}
+	log.Printf("Nonce: %d", nonce)
 	auth.Nonce = big.NewInt(int64(nonce))
 	return auth, nil
 }
